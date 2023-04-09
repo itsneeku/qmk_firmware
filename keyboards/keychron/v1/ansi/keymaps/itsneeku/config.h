f4372e1e38dcec2c357c2c8dd6237929090ff8af
neekudev <nicolae@rusu.dev>
2023-04-09 14:18:27 -0400

    update config headers with rgb matrix mode undefines

--- keyboards/keychron/v1/ansi/keymaps/itsneeku/config.h
+++ keyboards/keychron/v1/ansi/keymaps/itsneeku/config.h
@@ -1,51 +1,51 @@
-/* Copyright 2021 @ Keychron(https://www.keychron.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
-/* Undefine RGB Matrix Modes */
-#undef RGB_MATRIX_SOLID_COLOR 
+/* Selectively enable RGB Matrix Animation modes */
+// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+#undef ENABLE_RGB_MATRIX_ALPHAS_MODS
+#undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #undef ENABLE_RGB_MATRIX_BREATHING
+#undef ENABLE_RGB_MATRIX_BAND_SAT
+#undef ENABLE_RGB_MATRIX_BAND_VAL
+#undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
 #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
 #undef ENABLE_RGB_MATRIX_CYCLE_ALL
 #undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
 #undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-#undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
 #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
 #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#undef ENABLE_RGB_MATRIX_DUAL_BEACON
 #undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
 #undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-#undef ENABLE_RGB_MATRIX_DUAL_BEACON
 #undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#undef ENABLE_RGB_MATRIX_RAINDROPS
 #undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-#undef ENABLE_RGB_MATRIX_PIXEL_RAIN
 #undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
 #undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
 #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
 #undef ENABLE_RGB_MATRIX_SPLASH
+#undef ENABLE_RGB_MATRIX_MULTISPLASH
 #undef ENABLE_RGB_MATRIX_SOLID_SPLASH
-
-/* LED indices */
-#define MOUSE_JIGGLE_LED_INDEX 76
-#define QK_LOCK_LED_INDEX 77
+#undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
 
 /* Default RGB Matrix Animation Mode & Colour */
-#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define RGB_MATRIX_KEYPRESSES
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_REACTIVE_SIMPLE
 #define RGB_MATRIX_DEFAULT_HUE 190
 #define RGB_MATRIX_DEFAULT_SAT 255
 #define RGB_MATRIX_DEFAULT_VAL 255
+
+/* LED indices */
+#define MOUSE_JIGGLE_LED_INDEX 76
+#define QK_LOCK_LED_INDEX 77
